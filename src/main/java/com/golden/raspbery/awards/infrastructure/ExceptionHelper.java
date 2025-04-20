f43d20c8ffeb85f18b80776750fb91aaf1c9ff87
unknown <gabriedonascimento.2005@gmail.com>
2025-04-20 18:50:08 -0300

    feat: exceptions do MovieService

--- src/main/java/com/golden/raspbery/awards/infrastructure/ExceptionHelper.java
+++ src/main/java/com/golden/raspbery/awards/infrastructure/ExceptionHelper.java
@@ -19,18 +19,34 @@ public class ExceptionHelper {
 	}
 
 	public static BusinessException studioNotFound(Long id) {
-		return new BusinessException("O produtor de ID %d não foi encontrado.", id);
+		return new BusinessException("O estúdio de ID %d não foi encontrado.", id);
 	}
 
 	public static BusinessException studioNotSent() {
-		return new BusinessException("O produtor não foi enviado na requisição.");
+		return new BusinessException("O estúdio não foi enviado na requisição.");
 	}
 
 	public static BusinessException studioNameSentNotExistent() {
-		return new BusinessException("O produtor enviado não possui um nome.");
+		return new BusinessException("O estúdio enviado não possui um nome.");
 	}
 
 	public static BusinessException studioAlreadyExistent() {
-		return new BusinessException("O produtor enviado já existe.");
+		return new BusinessException("O estúdio enviado já existe.");
+	}
+
+	public static BusinessException movieNotFound(Long id) {
+		return new BusinessException("O filme de ID %d não foi encontrado.", id);
+	}
+
+	public static BusinessException movieAlreadyExistent(String title) {
+		return new BusinessException("O filme \"%s\" já existe.", title);
+	}
+
+	public static BusinessException movieNotSent() {
+		return new BusinessException("O produtor não foi enviado na requisição.");
+	}
+
+	public static BusinessException yearAlreadyHasWinnerMovie(int year, String winnerMovieTitle) {
+		return new BusinessException("O ano %d já possui um filme vencedor: %s");
 	}
 }
