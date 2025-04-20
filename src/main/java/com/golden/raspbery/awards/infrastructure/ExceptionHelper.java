b99e5c785cd9ae78c48f82dd221893df044b487c
unknown <gabriedonascimento.2005@gmail.com>
2025-04-19 22:46:19 -0300

    feat: exceptions do studio

--- src/main/java/com/golden/raspbery/awards/infrastructure/ExceptionHelper.java
+++ src/main/java/com/golden/raspbery/awards/infrastructure/ExceptionHelper.java
@@ -17,4 +17,20 @@ public class ExceptionHelper {
 	public static BusinessException producerAlreadyExistent() {
 		return new BusinessException("O produtor enviado já existe.");
 	}
+
+	public static BusinessException studioNotFound(Long id) {
+		return new BusinessException("O produtor de ID %d não foi encontrado.", id);
+	}
+
+	public static BusinessException studioNotSent() {
+		return new BusinessException("O produtor não foi enviado na requisição.");
+	}
+
+	public static BusinessException studioNameSentNotExistent() {
+		return new BusinessException("O produtor enviado não possui um nome.");
+	}
+
+	public static BusinessException studioAlreadyExistent() {
+		return new BusinessException("O produtor enviado já existe.");
+	}
 }
