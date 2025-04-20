be11d0598c6f10e48a2c064c02aae256990b6204
unknown <gabriedonascimento.2005@gmail.com>
2025-04-20 18:50:49 -0300

    feat: repositorio do filme

---
+++ src/main/java/com/golden/raspbery/awards/repositories/MovieRepository.java
@@ -0,0 +1,18 @@
+package com.golden.raspbery.awards.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.golden.raspbery.awards.models.Movie;
+
+public interface MovieRepository extends CrudRepository<Movie, Long>{
+
+	@Query("SELECT m FROM Movie m WHERE m.year = :year AND m.isWinner = :isWinner")
+	public List<Movie> listMoviesByYearAndIsWinner(@Param("year") int year, @Param("isWinner") boolean isWinner);
+
+	@Query("SELECT m FROM Movie m WHERE m.year = :year AND m.title = :title")
+	public Movie findMovieByYearAndTitle(@Param("year") int year, @Param("title") String title);
+}
