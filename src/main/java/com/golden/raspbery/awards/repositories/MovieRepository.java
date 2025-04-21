8fb36e94975799b028e99685c405264265a349ec
unknown <gabriedonascimento.2005@gmail.com>
2025-04-21 16:56:04 -0300

    feat: query para consulta de intervalos

--- src/main/java/com/golden/raspbery/awards/repositories/MovieRepository.java
+++ src/main/java/com/golden/raspbery/awards/repositories/MovieRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import com.golden.raspbery.awards.dtos.ProducerWinDTO;
 import com.golden.raspbery.awards.models.Movie;
 
 public interface MovieRepository extends CrudRepository<Movie, Long>{
@@ -15,4 +16,7 @@ public interface MovieRepository extends CrudRepository<Movie, Long>{
 
 	@Query("SELECT m FROM Movie m WHERE m.year = :year AND m.title = :title")
 	public Movie findMovieByYearAndTitle(@Param("year") int year, @Param("title") String title);
+
+	@Query(value = "SELECT new com.golden.raspbery.awards.dtos.ProducerWinDTO(p.name, m.year) FROM Movie m JOIN m.producersSet p WHERE m.isWinner = true ORDER BY p.name, m.year")
+	public List<ProducerWinDTO> findProducersWins();
 }
