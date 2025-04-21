8b1fb80df7315b0995b9f984927ca407ef73779c
unknown <gabriedonascimento.2005@gmail.com>
2025-04-21 16:56:22 -0300

    feat: removendo query desnecessaria

--- src/main/java/com/golden/raspbery/awards/repositories/ProducerRepository.java
+++ src/main/java/com/golden/raspbery/awards/repositories/ProducerRepository.java
@@ -1,7 +1,6 @@
 package com.golden.raspbery.awards.repositories;
 
-import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,8 +11,5 @@ import com.golden.raspbery.awards.models.Producer;
 public interface ProducerRepository extends CrudRepository<Producer, Long> {
 
 	@Query(value = "SELECT p FROM Producer p WHERE p.name = :name")
-	public Producer findProducerByName(@Param("name") String name);
-
-	@Query(value = "SELECT p FROM Producer p WHERE p.name IN (:nameList)")
-	public Set<Producer> findProducerByNameList(@Param("nameList") List<String> nameList);
+	public Optional<Producer> findProducerByName(@Param("name") String name);
 }
