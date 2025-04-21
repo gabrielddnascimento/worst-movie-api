58720d53b98382b2f8635006f2d148fea309d294
unknown <gabriedonascimento.2005@gmail.com>
2025-04-21 16:56:41 -0300

    feat: removido query desnecessaria

--- src/main/java/com/golden/raspbery/awards/repositories/StudioRepository.java
+++ src/main/java/com/golden/raspbery/awards/repositories/StudioRepository.java
@@ -1,7 +1,6 @@
 package com.golden.raspbery.awards.repositories;
 
-import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -12,8 +11,5 @@ import com.golden.raspbery.awards.models.Studio;
 public interface StudioRepository extends CrudRepository<Studio, Long>{
 
 	@Query("SELECT s FROM Studio s WHERE s.name LIKE :name")
-	public Studio findStudioByName(@Param("name") String name);
-
-	@Query(value = "SELECT s FROM Studio s WHERE s.name IN (:nameList)")
-	public Set<Studio> findStudioByNameList(@Param("nameList") List<String> nameList);
+	public Optional<Studio> findStudioByName(@Param("name") String name);
 }
